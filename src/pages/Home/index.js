fafd9839ca492d4622dd5917e4e15b0ca0c4b269
Akram Sabbah <akramsabbah9@gmail.com>
2021-07-16 19:44:06 -0700

    added Homepage

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (<>
+        <div>HEADER</div>
+        <main>MAIN</main>
+        <footer>FOOTER</footer>
+    </>);
+}
+
+export default Home;

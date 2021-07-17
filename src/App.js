757a893ec04a7638ba0e6850e53a2571d0ec56e4
Akram Sabbah <akramsabbah9@gmail.com>
2021-07-16 20:17:18 -0700

    edited spacing using flexbox

--- src/App.js
+++ src/App.js
@@ -5,31 +5,18 @@ import { Header } from "./components";
 import "./App.css";
 
 function App() {
-    return (<>
-        <Header />
-        <Router>
-            <Switch>
-                <Route path="/" component={Home} />
-            </Switch>
-        </Router>
-
-        {/* <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div> */}
-    </>);
+    return (
+        <div className="site-root">
+            <Header />
+            <div>
+                <Router>
+                    <Switch>
+                        <Route path="/" component={Home} />
+                    </Switch>
+                </Router>
+            </div>
+        </div>
+    );
 }
 
 export default App;

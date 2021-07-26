85cc418741fa6113ac0211db76646663c1820761
Akram Sabbah <akramsabbah9@gmail.com>
2021-07-26 01:53:08 -0700

    added Nav components to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,30 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header>
-            <Navbar>
-                <Navbar.Brand>ComputerLink</Navbar.Brand>
-                {/* <Nav sections={sections} current={current} setCurrent={setCurrent} /> */}
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand>ComputerLink</Navbar.Brand>
+                    <Nav>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <NavDropdown title="Servers">
+                            <NavDropdown.Item as={Link} to="/servers">TODO</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Workstations">
+                            <NavDropdown.Item as={Link}>TODO</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Solutions">
+                            <NavDropdown.Item as={Link}>TODO</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link as={Link}>About Us</Nav.Link>
+                        <Nav.Link as={Link}>Contact Us</Nav.Link>
+                        {/* Add search bar, cart, login/logout, and profile links */}
+                        {/* Remove About or Contact? Solutions? */}
+                    </Nav>
+                </Container>
             </Navbar>
         </header>
     );

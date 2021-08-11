afa44dd63c686b0315295f554fbe84934a94c1f1
Akram Sabbah <akramsabbah9@gmail.com>
2021-08-10 22:06:06 -0700

    added sessions to index

--- backend/index.js
+++ backend/index.js
@@ -3,27 +3,35 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const routes = require("./routes");
-// const db = require("./config/connection");
 
-const sess = {
-    secret: "Change this later",
-    cookie: { maxAge: 10 * 60 * 1000 }, // maxAge is 10 minutes
+const sessionOptions = {
+    cookie: {
+        maxAge: 24 * 60 * 1000, // 1 day
+        sameSite: true,
+        secure: "auto"
+    },
+    name: "computerlink-login",
     resave: false,
     rolling: true,
-    saveUninitialized: true,
-    // store: new SequelizeStore({
-    //     db: sequelize
-    // })
+    saveUninitialized: false,
+    secret: [process.env.SESSIONSECRET],
+    // store: new require('connect-pg-simple')(session))() // connect-pg-simple once in production
+    // see https://www.npmjs.com/package/connect-pg-simple
 };
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// // set session cookie to be secure in production, and trust reverse proxy
+// if (app.get("env") === "development") {
+//     sessionOptions.cookie.secure = true;
+//     app.set("trust proxy", 1);
+// }
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
-// app.use(session(sess));
+app.use(session(sessionOptions));
 
 app.get("/", (req, res) => res.json({message: "Hello World!"}));
 

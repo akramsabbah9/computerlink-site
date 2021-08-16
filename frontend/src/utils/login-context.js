50bc3d69dde67d82e094ef2f192dc4aad6d2ad18
Akram Sabbah <akramsabbah9@gmail.com>
2021-08-16 10:01:43 -0700

    read login cookie on startup

--- frontend/src/utils/login-context.js
+++ frontend/src/utils/login-context.js
@@ -1,12 +1,13 @@
 import React, { createContext, useContext } from "react";
 import { useLoginReducer } from "./reducers";
+import Auth from "./auth";
 
 const LoginContext = createContext();
 const { Provider } = LoginContext;
 
 const LoginProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useLoginReducer({
-        loggedIn: false
+        loggedIn: Auth.loggedIn() // initialize login state as true if cookie exists
     });
     return <Provider value={[state, dispatch]} {...props} />;
 };
